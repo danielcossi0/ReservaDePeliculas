f8bc04b18d8f960e7bfb259ab913e34bee6a9ee1
danielcossi0 <daniel.cossio@ceiba.com.co>
2021-07-17 15:49:36 -0500

    Creado el Schema para la tabla reserva en la base de datos

---
+++ microservicio/src/main/resources/db/migration/DDL/V1.1__schema.sql
@@ -0,0 +1,10 @@
+create table reserva (
+ idReserva int(11) not null auto_increment,
+ cedulaCliente varchar(15) not null,
+ nombreDeLaPelicula varchar(100) not null,
+ fechaDeReserva datetime null,
+ diasDeReserva int(1) not null,
+ fechaDeEntrega datetime null,
+ precioCalculado double null,
+ primary key (idReserva)
+);
