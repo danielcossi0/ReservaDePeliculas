8b88b5dd3816814badd9e9b89337f169c7d004bf
danielcossi0 <daniel.cossio@ceiba.com.co>
2021-07-19 16:58:15 -0500

    cambios menores

--- microservicio/dominio/src/test/java/com/ceiba/usuario/servicio/ServicioActualizarReservaTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/usuario/servicio/ServicioActualizarReservaTest.java
@@ -1,6 +1,9 @@
 package com.ceiba.usuario.servicio;
 
 import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
+import com.ceiba.usuario.modelo.entidad.Reserva;
+import com.ceiba.usuario.servicio.testdatabuilder.ReservaTestDataBuilder;
+
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -11,21 +14,32 @@ public class ServicioActualizarReservaTest {
 	@Test
 	public void devolverPeliculaAntesDeFechaDeEntregaActualizaPrecio() {
 		// arrange
-
+		Reserva reserva = new ReservaTestDataBuilder().build();
 		// act
-
+		
 		// assert
-
+		
 	}
 
 	@Test
 	public void devolverPeliculaDespuesDeFechaDeEntregaActualizaPrecio() {
 		// arrange
-
+		
 		// act
-
+		
 		// assert
 
+	}
+
+	@Test
+	public void eliminarReserva() {
+		// arrange
+		
+		// act
+		
+		// assert
+		
+		
 	}
 
 }
