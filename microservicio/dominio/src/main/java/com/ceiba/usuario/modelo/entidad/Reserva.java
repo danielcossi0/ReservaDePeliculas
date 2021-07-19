374878a1360ff8a70f3c3f6c57caa32c80827447
danielcossi0 <daniel.cossio@ceiba.com.co>
2021-07-19 11:37:19 -0500

    backup para borrar las clases de Usuario

--- microservicio/dominio/src/main/java/com/ceiba/usuario/modelo/entidad/Reserva.java
+++ microservicio/dominio/src/main/java/com/ceiba/usuario/modelo/entidad/Reserva.java
@@ -2,7 +2,6 @@ package com.ceiba.usuario.modelo.entidad;
 
 
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDate;
 
--- microservicio/dominio/src/test/java/com/ceiba/usuario/servicio/ServicioCrearReservaTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/usuario/servicio/ServicioCrearReservaTest.java
@@ -4,52 +4,73 @@ import com.ceiba.usuario.excepcion.ValidaLimiteReservas;
 import com.ceiba.usuario.modelo.entidad.Reserva;
 import com.ceiba.usuario.puerto.repositorio.RepositorioReserva;
 import com.ceiba.usuario.servicio.testdatabuilder.ReservaTestDataBuilder;
+
+import junit.framework.Assert;
+
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import com.ceiba.BasePrueba;
+import com.ceiba.dominio.excepcion.ExcepcionValorInvalido;
 
 public class ServicioCrearReservaTest {
 
 	private static final String EL_USUARIO_YA_TIENE_TRES_RESERVAS_PENDIENTES_SIN_ENTREGAR = "El usuario ya tiene 3 reservas pendientes sin entregar";
-
-	
-	
+	private static final String LA_RESERVA_DEBE_SER_MENOR_A_5_DIAS = "Los dias de reserva deben ser menores a 5";
 
 	@Test(expected = AssertionError.class)
 	public void alquilarPeliculaConLimiteDeReservas() {
 
 		// arrange
-		
+
+		Reserva reserva = new ReservaTestDataBuilder().build();
+		RepositorioReserva repositorioReserva = Mockito.mock(RepositorioReserva.class);
+		ServicioCrearReserva servicioCrearReserva = new ServicioCrearReserva(repositorioReserva);
+
+		// act
+
+		Mockito.when(repositorioReserva.crear(Mockito.any())).thenReturn(1L);
+		// assert
+
+		BasePrueba.assertThrows(() -> servicioCrearReserva.ejecutar(reserva), ValidaLimiteReservas.class,
+				EL_USUARIO_YA_TIENE_TRES_RESERVAS_PENDIENTES_SIN_ENTREGAR);
+	}
+
+	@Test(expected = AssertionError.class)
+	public void calcularFechaDeEntregaDebeSerMenorA5Dias() {
+		// arrange
+
+		Reserva reserva = new ReservaTestDataBuilder().build();
+		RepositorioReserva repositorioReserva = Mockito.mock(RepositorioReserva.class);
+		ServicioCrearReserva servicioCrearReserva = new ServicioCrearReserva(repositorioReserva);
+
+		// act
+
+		Mockito.when(repositorioReserva.crear(Mockito.any())).thenReturn(1L);
+		// assert
+
+		BasePrueba.assertThrows(() -> servicioCrearReserva.ejecutar(reserva), ExcepcionValorInvalido.class,
+				LA_RESERVA_DEBE_SER_MENOR_A_5_DIAS);
+	}
+
+	@Test
+	public void alquilarPeliculaCorrectamente() {
+		// arrange
 		Reserva reserva = new ReservaTestDataBuilder().build();
 		RepositorioReserva repositorioReserva = Mockito.mock(RepositorioReserva.class);
 		ServicioCrearReserva servicioCrearReserva = new ServicioCrearReserva(repositorioReserva);
 		
-		//act 
+		// act
+		Mockito.when(repositorioReserva.crear(Mockito.any())).thenReturn(1L);
 		
-		Mockito.when(repositorioReserva.crear(Mockito.any())).thenReturn(1L);		
-		//assert
-
-		BasePrueba.assertThrows(() -> servicioCrearReserva.ejecutar(reserva), ValidaLimiteReservas.class,
-				EL_USUARIO_YA_TIENE_TRES_RESERVAS_PENDIENTES_SIN_ENTREGAR);}
-
-	public void calcularFechaDeEntregaDebeSerMenorA5Dias() {
-		// arrange
-
-		// act
-
+		
 		// assert
-
+		
+		
+		assertEquals(1L, servicioCrearReserva.ejecutar(reserva), 0.0);
+		 
 	}
-
-	public void calcularPrecioDeReservaSumandoDiezMilDiarios() {
-		// arrange
-
-		// act
-
-		// assert
-
-	}
-
 
 }
